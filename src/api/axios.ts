86f1dd9d1fbd675e678da9bc99cf318e9ff278be
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-12-14 19:36:25 -0300

    fix: correct dotenv use

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`;
+const baseURL = `${import.meta.env.VITE_REACT_APP_BASE_URL}:${import.meta.env.VITE_REACT_APP_PORT}`;
 
 export default axios.create({
   baseURL
