ce26eaa8f3b54cfab83ffbc9eed285ad0767734d
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-12-14 11:22:41 -0300

    fix: change axios requisition to .env path

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const baseURL = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`;
+
 export default axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL
 })
